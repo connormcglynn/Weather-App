b2db2198c82d53495e93a439901ca06cfdda206d
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-22 13:24:27 -0400

    finished all requirements, ready to tidy/refactor

--- script.js
+++ script.js
@@ -11,9 +11,7 @@ $(document).ready(function() {
                 currentQuote = 0;
             } else {
                 currentQuote++;
-            }
-    
-            $(allQuotes[currentQuote]).fadeIn(400);
+            }   $(allQuotes[currentQuote]).fadeIn(400);
         });
     }
 
@@ -22,28 +20,29 @@ $(document).ready(function() {
 });
 
 // set up variables for JSON population
-
 const loc = document.getElementById("location");
 const tempNum = document.getElementById("temperature-num");
 const tempScale = document.getElementById("temperature-scale");
 const weatherCond = document.getElementById("weather-condition");
 const weatherIcon = document.getElementById("weather-icon");
 
-// get user's geolocation & change the background 
-
+// get user's geolocation 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            getWeather(position.coords.latitude,
-                       position.coords.longitude);
+            getWeather(position.coords.latitude, position.coords.longitude);
         });
     } else {
         loc.innerHTML = "Could not get geolocation from browser."
     }
 }
 
-// Once geolocation is acquired, get weather for that location
+// Run the function to get user's geolocation
+window.onload = function() {
+    getLocation();
+  };
 
+// Once geolocation is acquired, get weather for that location
 function getWeather(lat, lon) {
     const api = "https://fcc-weather-api.glitch.me/api/current?";
     fetch(`${api}lat=${lat}&lon=${lon}`, {method: "get"})
@@ -52,35 +51,9 @@ function getWeather(lat, lon) {
             updateDataToUI(data.name, data.weather, data.main.temp);
         })
         .catch(function(err) {
-            console.error(err);
+            console.log(err);
           });
-
-          function backgroundCycle (){
-            if ($("#weather-condition").text() == "Clouds" || "Clear"){
-                $("body").css("background-image", "url(./images/clouds.jpg)");
-                $(document).ready(function() {
-                    var pixelToMove = 50;
-                    $("background-image").mousemove(function(e) {
-                    var width = $(this).innerWidth();
-                    var height = $(this).innerHeight();
-                    var newValueX = (e.pageX / width) * pixelToMove;
-                    var newValueY = (e.pageY / height) * pixelToMove;
-                    $(this).css('background-position', newValueX + '%' + ' ' + newValueY + '%');
-                    });
-                    });
-            } else {
-                $("body").css("background-color", "black");
-                }
-            }
-        
-            backgroundCycle();
 };
-
-
-// Run the function to get user's geolocation
-window.onload = function() {
-    getLocation();
-  };
 
 // Update the data from API to DOM
 function updateDataToUI(location, weather, temp) {
@@ -88,48 +61,52 @@ function updateDataToUI(location, weather, temp) {
     weatherCond.innerHTML = weather[0].main;
     loc.innerHTML = location;
     tempNum.innerHTML = `${temp}`;
-  };
 
-// Parallax background on mousemove
+    // Celsius --> Fahrenheit toggle
+    $(".btn").click(function(){
+        var celsius = temp;
+        var fahrenheit = ((celsius*1.8) + 32);
+        var degrees = $("#temperature-num").text();
+        if (degrees == celsius) {
+            $("#temperature-num").text(fahrenheit.toFixed(2));
+        } else {
+            return $("#temperature-num").text(celsius.toFixed(2));
+        }   
+    });
+        $(".btn").on('click', function () {
+         var text=$('.btn').text();
+         if(text === "Convert to °F"){
+           $(this).html('Convert to °C');
+         } else {
+           $(this).html('Convert to °F');
+        }
+       });
+       
+};
+    
 
-// $("body").mousemove(function(e) {
-//     parallaxIt(e, ".slide", -100);
-//     parallaxIt(e, "img", -30);
-//   });
-  
-//   function parallaxIt(e, target, movement) {
-//     var $this = $("body");
-//     var relX = e.pageX - $this.offset().left;
-//     var relY = e.pageY - $this.offset().top;
-  
-//     TweenMax.to(target, 1, {
-//       x: (relX - $this.width() / 2) / $this.width() * movement,
-//       y: (relY - $this.height() / 2) / $this.height() * movement
-//     });
-//   }
-
-(function($){
-    $.fn.backgroundMove = function (options) {
-        var defaults = {movementStrength:'50'}
-        var options = $.extend(defaults,options);
-        var $this = $(this);
+// Background motion on mousemove
+const backgroundMove = (function ($) {
+    $.fn.backgroundMover = function (options) {
+        var defaults = { movementStrength: '50' };
+        var options = $.extend(defaults, options);
         var movementStrength = options.movementStrength;
         var height = movementStrength / $(window).height();
         var width = movementStrength / $(window).width();
         
-        $this.mousemove(function(e){
-        var pageX = e.pageX - ($(window).width() / 2);
-        var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 25;
-        var newvalueY = height * pageY * -1 - 50;
-                $this.css("background-position", newvalueX+"px     "+newvalueY+"px");
+        $(this).mousemove(function (e) {
+            var pageX = e.pageX - ($(window).width() / 2);
+            var pageY = e.pageY - ($(window).height() / 2);
+            var newvalueX = width * pageX * -1 - 25;
+            var newvalueY = height * pageY * -1 - 50;
+            $(this).css("background-position", newvalueX + "px " + newvalueY + "px");
         });
+    };
+});
 
-    }
-})(jQuery);
-$('body').backgroundMove({
-                movementStrength:'50'
-            });
+backgroundMove($);
 
-            var scene = document.getElementById('scene');
-            var parallaxInstance = new Parallax(scene);
+$('body').backgroundMover({
+    movementStrength:'50'
+});
+
